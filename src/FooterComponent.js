5d71e9a4290cd0fa51dba97383d605dbe502db5c
taruna.bishnoi <taruna.bishnoi@servicenow.com>
2022-05-01 18:44:28 +0530

    Created document-management-app

---
+++ src/FooterComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FooterComponent = ({ tableDocuments }) => {
+  return (
+    <div className="footer-container">
+     @Copyrights | All rights reserved | Imprints | Terms of use | Privacy policy
+    </div>
+  );
+};
+
+export default FooterComponent
