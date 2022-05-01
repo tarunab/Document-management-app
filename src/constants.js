fcd970219ebc25dcfbbd012682c22c937a4547d4
taruna.bishnoi <taruna.bishnoi@servicenow.com>
2022-05-01 18:51:58 +0530

    Remove files

--- src/constants.js
+++
@@ -1,3 +0,0 @@
-export const DOCUMENTS_ENDPOINT = 'https://frontend.apply.crosslend.dev/documents.json';
-export const USERS_ENDPOINT = 'https://frontend.apply.crosslend.dev/user/info.json';
-export const ROWS_PER_PAGE = 4;
